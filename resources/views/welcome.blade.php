d5c886da8b63feb6b10f2fadc6fff99cff1d72cb
fabio <fabio@fabio.com>
2024-09-27 21:24:08 -0400

    cambiar label

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -402,7 +402,7 @@
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar</a>
                     @endif
                 @endauth
             </div>
